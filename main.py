55ace193251265f174372ea12a52dff927778ec5
Klaudia <kklaudia.maciaszek@gmail.com>
2023-04-26 18:44:21 +0200

    Using Nearest Neighbors Algorithm and calculating how many examples are needed to be add in miniority class

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import sklearn as sklearn
 from sklearn.datasets import make_classification
-from sklearn.metrics import accuracy_score
 
-    # Generating data
+import numpy as np
+from collections import Counter
+from sklearn.neighbors import NearestNeighbors
+
+# Generating data
 x, y = sklearn.datasets.make_classification(n_samples=200, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, weights=[0.1, 0.9])
+
+    # Adasyn
+def Adasyn(x, y, weigts):
+        # Finding the miniority class
+    numberOfExamples = Counter(y)
+    miniorityClassSamples = min(numberOfExamples.values())
+
+        # Calculating how many examples are needed to be add in miniority class
+    majorityClassSamples = max(numberOfExamples.values())
+    examplesToAdd = int(weigts*(majorityClassSamples-miniorityClassSamples))
+
+        # Creating the List for miniority class
+    miniorityList = np.where(y == 0)[0]
+    print(miniorityList)
+
+        # Using Nearest Neighbors algorithm
+    nbrs = NearestNeighbors().fit(x, y)
+    nbrsOfExample = nbrs.kneighbors(x[miniorityList], return_distance=False)
+    print(nbrsOfExample)
+
+        # Generaiting examples in miniority class
+    #for i in range(examplesToAdd):
+        #to do
+
+Adasyn(x, y, 0.9)
