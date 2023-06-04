503ef657ed373cb57748d92b35710e7ba0560bd5
Klaudia <kklaudia.maciaszek@gmail.com>
2023-06-04 12:10:45 +0200

    Adding corrections

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
 from imblearn.over_sampling import ADASYN, SMOTE, BorderlineSMOTE
 from ImplementedAdasyn import ImplementedAdasyn
-import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
-import openpyxl
 
 print("\nImplementacja metody Adasyn\n")
 
@@ -30,8 +28,6 @@ plt.title('Dane syntetyczne przed oversamplingiem')
 plt.savefig('Dane_syntetyczne_przed_oversamplingiem.png')
 
 # Real data
-excelData = pd.read_excel('data.xlsx', engine='openpyxl')
-csvData = excelData.to_csv('dane_rzeczywiste.csv', index=False, header=False)
 realData = np.loadtxt('dane_rzeczywiste.csv', delimiter=',')
 real_x = realData[:, :-1]
 real_y = realData[:, -1]
@@ -44,57 +40,6 @@ iada = ImplementedAdasyn()
 ada = ADASYN()
 br = BorderlineSMOTE()
 sm = SMOTE()
-
-
-def testing_for_ImplementedAdasyn(x, y, method, skfold, data):
-    # Empty arrays for metric scores
-    accArray = []
-    precArray = []
-    f1Array = []
-    recArray = []
-
-    method_x, method_y = method._fit_resample(x, y)
-    print(f" | Po zaimportowanym {method}: ", Counter(method_y), "\n-- dla każdego folda po kolei:")
-
-    # Division into training and testing sets
-    for i, (train_index, test_index) in enumerate(skfold.split(x, y)):
-        # Using KNeighborsClassifier
-        knc = KNeighborsClassifier(n_neighbors=5)
-
-        x_train, y_train = x[train_index], y[train_index]
-        # x_test, y_test = x[test_index], y[test_index]
-
-        x_train_resample, y_train_resample = method._fit_resample(x_train, y_train)
-        knc.fit(x_train_resample, y_train_resample)
-        y_predict = knc.predict(x[test_index])
-
-        # Using accuracy_score, precision_score, f1_score and recall_score
-        acc = accuracy_score(y[test_index], y_predict)
-        prec = precision_score(y[test_index], y_predict)
-        f1 = f1_score(y[test_index], y_predict)
-        rec = recall_score(y[test_index], y_predict)
-
-        accArray.append(acc)
-        precArray.append(prec)
-        f1Array.append(f1)
-        recArray.append(rec)
-
-    print(f"-- dokładność {method} wynosi:", accArray, ", natomiast odchylenie", np.average(accArray))
-    print(f"-- precyzja {method} wynosi:", precArray, ", natomiast odchylenie", np.average(precArray))
-    print(f"-- f1 {method} wynosi:", f1Array, ", natomiast odchylenie", np.average(f1Array))
-    print(f"-- recall {method} wynosi:", recArray, ", natomiast odchylenie", np.average(recArray))
-
-    np.save(f'dokladnosc{method}.npy', accArray)
-    np.save(f'precyzja{method}.npy', precArray)
-    np.save(f'f1{method}.npy', f1Array)
-    np.save(f'recall{method}.npy', recArray)
-
-    shapiro_test = stats.shapiro(method_x)
-    print(shapiro_test, "\n")
-
-    plt.scatter(method_x[:, 0], method_x[:, 1], c=method_y)
-    plt.title(f'Dane {data} po zaimportowanym {method}')
-    plt.savefig(f'Dane_{data}_po_zaimportowanym_{method}.png')
 
 
 def testing(x, y, method, skfold, data):
@@ -149,13 +94,13 @@ def testing(x, y, method, skfold, data):
 
 
 # Testing methods for synthetic data
-testing_for_ImplementedAdasyn(x, y, iada, skf, synt)
+testing(x, y, iada, skf, synt)
 testing(x, y, ada, skf, synt)
 testing(x, y, sm, skf, synt)
 testing(x, y, br, skf, synt)
 
 # Testing methods for real data
-testing_for_ImplementedAdasyn(real_x, real_y, iada, skf, real)
+testing(real_x, real_y, iada, skf, real)
 testing(real_x, real_y, ada, skf, real)
 testing(real_x, real_y, sm, skf, real)
 testing(real_x, real_y, br, skf, real)
